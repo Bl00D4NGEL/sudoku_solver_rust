aba2dd7da105a99bcc74d65e2961c6afdff3c314
Bl00D4NGEL <kuhlesdominik@gmx.de>
2022-09-10 23:52:57 +0200

    Early return if number of possibilities is != 1

--- src/main.rs
+++ src/main.rs
@@ -75,12 +75,14 @@ impl Row {
 
 fn run(row: Row) -> Row {
     let possibilities = row.get_possibilities();
+    if possibilities.len() != 1 {
+        return row;
+    }
+
     let mut fields = row.fields;
-    if possibilities.len() == 1 {
-        for field in &mut fields {
-            if field.is_empty() {
-                field.update(possibilities[0])
-            }
+    for field in &mut fields {
+        if field.is_empty() {
+            field.update(possibilities[0])
         }
     }
     return Row::new(fields);
