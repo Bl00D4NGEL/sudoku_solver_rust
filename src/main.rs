a7c28361be63e08c967f880b3abb6d0b9702cd7e
Bl00D4NGEL <kuhlesdominik@gmx.de>
2022-09-12 23:33:52 +0200

    Added easy example from sudoku.com and re-add SolveByColumns/Rows

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::{fs, io::Error};
+use std::{fs, io::Error, time::Duration};
 
 use sudoku_solver::{
     printable::Printable,
-    solvable::{ByPossibilities, Solvable},
+    solvable::{ByColumns, ByPossibilities, ByRows, Solvable},
     sudoku::{Field, Grid},
 };
 
@@ -21,10 +21,16 @@ fn solve(grid: &mut Grid) -> &mut Grid {
     grid.update_possibilities_in_columns();
 
     solve_with(grid, ByPossibilities {});
+    solve_with(grid, ByColumns {});
+    solve_with(grid, ByRows {});
 
     if grid.is_solved() {
         return grid;
     }
+
+    println!("Grid not solved");
+    grid.print();
+    std::thread::sleep(Duration::new(1, 0));
 
     return solve(grid);
 }
@@ -34,7 +40,7 @@ fn solve_with<'a>(grid: &'a mut Grid, solver_solvable: impl Solvable) -> &mut Gr
 }
 
 fn create_grid() -> Result<Grid, Error> {
-    let file_content = fs::read_to_string("./grid5.txt")?;
+    let file_content = fs::read_to_string("./grid6.txt")?;
 
     let mut grid = Grid::create_empty();
     for (row, line) in file_content.lines().into_iter().enumerate() {
--- src/solvable.rs
+++ src/solvable.rs
@@ -4,6 +4,79 @@ pub trait Solvable {
     fn solve<'a>(&self, grid: &'a mut Grid) -> &'a mut Grid;
 }
 
+pub struct ByRows {}
+
+impl Solvable for ByRows {
+    fn solve<'a>(&self, grid: &'a mut Grid) -> &'a mut Grid {
+        for row in 0..=8 {
+            let cloned_grid = grid.clone();
+            let fields = match cloned_grid.get_fields_in_row(row) {
+                Ok(v) => v,
+                Err(_) => continue,
+            };
+
+            let missing_digit = match determine_missing_digit_in_fields(&fields) {
+                Some(d) => d,
+                None => continue,
+            };
+
+            for (column, field) in fields.iter().enumerate() {
+                if field.is_empty() {
+                    grid.set_field(row, column, Field::new(missing_digit));
+                }
+            }
+        }
+
+        grid
+    }
+}
+
+fn determine_missing_digit_in_fields(fields: &Vec<&Field>) -> Option<i32> {
+    let used_digits: Vec<i32> = fields
+        .iter()
+        .map(|f| f.value().unwrap_or(0))
+        .filter(|f| f.gt(&0))
+        .collect();
+
+    if used_digits.len() != 8 {
+        return Option::None;
+    }
+
+    for digit in 1..=9 {
+        if !used_digits.contains(&digit) {
+            return Option::Some(digit);
+        }
+    }
+
+    return Option::None;
+}
+pub struct ByColumns {}
+
+impl Solvable for ByColumns {
+    fn solve<'a>(&self, grid: &'a mut Grid) -> &'a mut Grid {
+        for column in 0..=8 {
+            let cloned_grid = grid.clone();
+            let fields = match cloned_grid.get_fields_in_column(column) {
+                Ok(v) => v,
+                Err(_) => continue,
+            };
+
+            let missing_digit = match determine_missing_digit_in_fields(&fields) {
+                Some(d) => d,
+                None => continue,
+            };
+
+            for (row, field) in fields.iter().enumerate() {
+                if field.is_empty() {
+                    grid.set_field(row, column, Field::new(missing_digit));
+                }
+            }
+        }
+
+        grid
+    }
+}
+
 pub struct ByPossibilities {}
 
 impl Solvable for ByPossibilities {
