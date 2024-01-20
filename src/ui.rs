dd9bc3bce9b5f20a040734f6ab339bbaa5d3062d
Dominik Peters <kuhlesdominik@gmx.de>
2024-01-20 19:35:24 +0100

    refac: split ui rendering into impl of Field

--- src/ui.rs
+++ src/ui.rs
@@ -32,10 +32,10 @@ impl SudokuGrid {
             .size(Size::relative(1.0))
             .vertical(|mut upper_strip| {
                 upper_strip.cell(|ui| {
-                    self.draw_grid(ui, 9, 9, |field_strip, row_idx, col_idx| {
+                    draw_grid(ui, 9, 9, |field_strip, row_idx, col_idx| {
                         field_strip.cell(|ui| {
                             if let Some(field) = self.get_field(row_idx, col_idx) {
-                                let p = self.draw_field(field, ui);
+                                let p = field.ui(ui);
                                 if let Some(solve_step) = p {
                                     changes.push(((field.row, field.column), solve_step));
                                 }
@@ -49,8 +49,8 @@ impl SudokuGrid {
     }
 }
 
-impl SudokuGrid {
-    fn draw_field(&self, field: &Field, ui: &mut egui::Ui) -> Option<SolveStep> {
+impl Field {
+    fn ui(&self, ui: &mut egui::Ui) -> Option<SolveStep> {
         let dark_mode = ui.visuals().dark_mode;
         let faded_color = ui.visuals().window_fill();
         let faded_color = |color: Color32| -> Color32 {
@@ -61,28 +61,29 @@ impl SudokuGrid {
 
         let mut solve_step: Option<SolveStep> = None;
 
-        match field.value {
+        match self.value {
             None => {
-                let color = if field.possibilities.len() == 1 {
+                let color = if self.possibilities.len() == 1 {
                     faded_color(Color32::BLUE)
                 } else {
                     faded_color(Color32::RED)
                 };
 
-                if field.possibilities.is_empty() {
+                if self.possibilities.is_empty() {
                     panic!(
                         "No field value nor possibilities exist for {} / {}",
-                        field.row, field.column
+                        self.row, self.column
                     );
                 }
+
                 ui.painter()
                     .rect_filled(ui.available_rect_before_wrap(), 0.0, color);
 
-                self.draw_grid(ui, 3, 3, |field_strip, row, col| {
+                draw_grid(ui, 3, 3, |field_strip, row, col| {
                     field_strip.cell(|ui| {
                         let possibility = row * 3 + col + 1;
                         ui.centered_and_justified(|ui| {
-                            if field.possibilities.contains(&possibility) {
+                            if self.possibilities.contains(&possibility) {
                                 let response = ui.label(&possibility.to_string());
 
                                 let response = response.interact(egui::Sense::click());
@@ -116,30 +117,25 @@ impl SudokuGrid {
 
         solve_step
     }
+}
 
-    fn draw_grid<F>(
-        &self,
-        ui: &mut egui::Ui,
-        row_count: usize,
-        column_count: usize,
-        mut field_fn: F,
-    ) where
-        F: FnMut(&mut Strip, usize, usize),
-    {
-        StripBuilder::new(ui)
-            .sizes(Size::remainder(), row_count)
-            .vertical(|mut strip| {
-                for row in 0..row_count {
-                    strip.strip(|row_builder| {
-                        row_builder
-                            .sizes(Size::remainder(), column_count)
-                            .horizontal(|mut field_strip| {
-                                for column in 0..column_count {
-                                    field_fn(&mut field_strip, row, column);
-                                }
-                            });
-                    });
-                }
-            });
-    }
+fn draw_grid<F>(ui: &mut egui::Ui, row_count: usize, column_count: usize, mut field_fn: F)
+where
+    F: FnMut(&mut Strip, usize, usize),
+{
+    StripBuilder::new(ui)
+        .sizes(Size::remainder(), row_count)
+        .vertical(|mut strip| {
+            for row in 0..row_count {
+                strip.strip(|row_builder| {
+                    row_builder
+                        .sizes(Size::remainder(), column_count)
+                        .horizontal(|mut field_strip| {
+                            for column in 0..column_count {
+                                field_fn(&mut field_strip, row, column);
+                            }
+                        });
+                });
+            }
+        });
 }
